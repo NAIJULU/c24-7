42d0772ef211ecc6d336793d73ead82efc224a79
Emerson Gutierrez <emergugue@gmail.com>
2014-04-04 16:40:31 -0500

    Se le dio estilo al pronostico de lluvia

--- wp-content/themes/clima/widgets/widget-pronostico.php
+++ wp-content/themes/clima/widgets/widget-pronostico.php
@@ -50,34 +50,34 @@
 	    <div class="<?php if($i) echo 'active';$i=0?> item">
 	        <h2>Pronóstico de <?php echo $ciudad ?></h2>
 		        <div class="row-fluid">
-		          <div class="span6">
+		          <div class="span7">
 	    	        <div class="row-fluid clearfix">
 	        	      <div class="span12"> <span class="dia">Hoy</span> <span class="mes"><?php echo $this->meses[date('n')]; ?> </span> <span class="dias"><?php echo strftime("%d", $this->hoy); ?> </span> </div>
 	              <div class="span12 lluvias"> <span class="titulo2">Pronóstico Lluvia</span>
 	                <div class="row-fluid">
-	               	  <div class="span4">
+	               	  <div class="span3">
 	                    
-	                      <div class="night">Madrugada</div>
+	                      <div class="pronostico-item">Madrugada</div>
 	                      <div class="numMax"><?php echo get_option('LluvMad'.$key) ?></div>
 	                    
 	                  </div>
 
-	                  <div class="span4">
+	                  <div class="span3">
 	                    
-	                      <div class="tempMax">Mañana</div>
+	                      <div class="pronostico-item">Mañana</div>
 	                      <div class="numMax"><?php echo get_option('LluvMan'.$key) ?></div>
 	                    
 	                  </div>
-	                  <div class="span4">
+	                  <div class="span3">
 	                    
-	                      <div class="tempMin">Tarde</div>
-	                      <div class="numMin"><?php echo get_option('LluvTar'.$key) ?></div>
+	                      <div class="pronostico-item">Tarde</div>
+	                      <div class="numMax"><?php echo get_option('LluvTar'.$key) ?></div>
 	                    
 	                  </div>
-	                  <div class="span4">
+	                  <div class="span3">
 	                    
-	                      <div class="night">Noche</div>
-	                      <div class="numMin"><?php echo get_option('LluvNoc'.$key) ?></div>
+	                      <div class="pronostico-item">Noche</div>
+	                      <div class="numMax"><?php echo get_option('LluvNoc'.$key) ?></div>
 	                    
 	                  </div>
 	                </div>
@@ -99,7 +99,11 @@
 	              </div>
 	            </div>
 	          </div>
-	          <div class="span6 mapa-ciudades"> <img src=<?php echo bloginfo('wpurl')."/wp-content/uploads/mapas/mapa_".ucfirst($key).".png"  ?> />   </div>
+	          <div class="span5 mapa-ciudades">
+	          	<figure>
+	          		<img src=<?php echo bloginfo('wpurl')."/wp-content/uploads/mapas/mapa_".ucfirst($key).".png"  ?> />   
+	          	</figure> 
+	          </div>
 	        </div>
 	      </div>
 	    <?php endforeach?>
