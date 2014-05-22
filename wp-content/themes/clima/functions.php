992af068360e0bbc4e71e967312caa699ad02ca7
Pablo Martínez <pablo.576@hotmail.com>
2014-05-21 20:33:33 -0500

    retoques

--- wp-content/themes/clima/functions.php
+++ wp-content/themes/clima/functions.php
@@ -916,64 +916,50 @@ add_action( 'widgets_init', 'creaWidgets' );
 
 function loadBlogs()
 {
+  
+  $paged   = ( isset($_GET['paged']) ) ? $_GET['paged'] : 1;
+  $content = "";
+  $i = 0;
+  $args = array('cat'=>'2', 'paged'=> $paged );
+  $the_query = new WP_Query($args); 
 
-$paged   = ( isset($_GET['paged']) ) ? $_GET['paged'] : 1;
-$content = "";
-$i = 0;
-$the_query = new WP_Query( 'paged=' . $paged ); 
+  if ($the_query->have_posts())
+  {
+    while ($the_query->have_posts()) : $the_query->the_post(); 
+      $categoria    = get_the_category();
+      $categoria    = ( !empty($categoria[1]->name) ) ? $categoria[1]->name : $categoria[0]->name ; 
+      $url          = wp_get_attachment_url( get_the_post_thumbnail($post->ID,'medium'));
+      $url          = (!empty($url)) ? $url : get_template_directory_uri().'/images/dummie-post.png';
+      $contenuto    = get_the_content();
 
-if ($the_query->have_posts())
-{
+      $pClass = "";
 
+      foreach (get_post_class(array('clearfix')) as $post_cass) 
+      {
+        $pClass .= $post_cass." ";
+      }
 
-while ($the_query->have_posts() ) : $the_query->the_post(); 
-
-
-// 2 -> categoria blogs
-if(in_category(2)) :
-
-$categoria    = get_the_category();
-$categoria    = ( !empty($categoria[1]->name) ) ? $categoria[1]->name : $categoria[0]->name ; 
-$url = wp_get_attachment_url( get_the_post_thumbnail($post->ID,'medium') );
-$url = (!empty($url)) ? $url : get_template_directory_uri().'/images/dummie-post.png';
-$contenuto = get_the_content();
-
-$pClass = "";
-
-foreach (get_post_class(array('clearfix')) as $post_cass) 
-{
-  $pClass .= $post_cass." ";
-}
-
-
-
-$content .='<article id="post-'.get_the_ID().'"  class="'. $pClass.' isotope-item" role="article" class="blog-thumb">
-          <a href="'.get_permalink().'" rel="bookmark" title="'.the_title('','',false).'">
-                 '.in_category($blogId).'
-                <figure><img src="'.$url.'" alt="'.the_title('','',false).'" class="thumb" /></figure>
-                <div class="contenido"><header ><span class="categorias">'.$categoria.'</span>
-                  <h1>'.the_title('','',false).'</h1>
-                  <time datetime="'.get_the_time('Y-m-j').'" pubdate>'.get_the_time('j').' de '.get_the_time('F').' del '.get_the_time('Y').'</time>
-                </header>
-                <p>'.substr(wp_filter_nohtml_kses( $contenuto ), 0,80).'...'.'
-                <span>Leer Más +<span></p></div>
-            </a>
-          </article>';
-          
-    $i++;
-    endif;
+      $content .= '<article id="post-'.get_the_ID().'"  class="'. $pClass.' isotope-item" role="article" class="blog-thumb">
+      <a href="'.get_permalink().'" rel="bookmark" title="'.the_title('','',false).'">
+       <figure><img src="'.$url.'" alt="'.the_title('','',false).'" class="thumb" /></figure>
+          <div class="contenido"><header ><span class="categorias">'.$categoria.'</span>
+          <h1>'.the_title('','',false).'</h1>
+          <time datetime="'.get_the_time('Y-m-j').'" pubdate>'.get_the_time('j').' de '.get_the_time('F').' del '.get_the_time('Y').'</time>
+        </header>
+        <p>'.substr(wp_filter_nohtml_kses( $contenuto ), 0,80).'...'.'
+        <span>Leer Más +<span></p></div>
+      </a>
+      </article>';    
+      $i++;
   endwhile;
-}
+  }
 
-echo $content;
-die();
+  echo $content;
+  die();
 }
-
 
 add_action('wp_ajax_nopriv_getBlog', 'loadBlogs');
 add_action('wp_ajax_getBlog', 'loadBlogs');
-
-
 
 
 function loadGallery()
@@ -982,16 +968,13 @@ function loadGallery()
 $paged   = ( isset($_GET['paged']) ) ? $_GET['paged'] : 1;
 $content = "";
 $i = 0;
-$the_query = new WP_Query( 'paged=' . $paged ); 
+$the_query = new WP_Query('paged=' . $paged ); 
 
 if ($the_query->have_posts())
 {
 
 
 while ($the_query->have_posts() ) : $the_query->the_post(); 
-
-
-  // 2 -> categoria blogs
   if(in_category(23)) :
 
       $categoria    = get_the_category();
