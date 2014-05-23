8f7cd5091ccb3b2118619dad421a4c08881f8fe6
Emerson Gutierrez <emergugue@gmail.com>
2014-05-23 12:38:31 -0500

    Se dio estilo al formulario de contacto para móviles

--- wp-content/themes/clima/contacto.php
+++ wp-content/themes/clima/contacto.php
@@ -5,24 +5,26 @@ Template Name: contacto
 ?>
 <?php get_header(); ?>
 
-    <div id="content" class="clearfix row-fluid">
-        <div id="main" class="span12 clearfix" role="main">
-            <div id="main-content" class="span8">
-              <section class="post_content">
-                <div class="row-fluid clearfix">
-                  <div class="span12">
-                    <header>
-                      <div class="page-header"><h1>Contacto</h1></div>
-                    </header>
-                  </div>
-                  <?php echo do_shortcode('[contact-form-7 id="407" title="Formulario de contacto 1"]'); ?>
-                </div>
-              </section>
-            </div>
-            <div id="slidebar-der" class="span4">
-              <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('slidebar_derecha') ) : ?>
-              <?php endif; ?>
-            </div>
-      </div>
-  </div>      
+<div id="content" class="clearfix row-fluid">
+  <div id="main" class="span12 clearfix" role="main">
+    <div id="main-content" class="span8">
+      <section class="post_content">
+        <div class="row-fluid clearfix">
+          <div class="span12">
+            <header>
+              <div class="page-header"><h1>Contacto</h1></div>
+            </header>
+          </div>
+          <div class="formulario-contacto">
+            <?php echo do_shortcode('[contact-form-7 id="407" title="Formulario de contacto 1"]'); ?>
+          </div>
+        </div>
+      </section>
+    </div>
+    <div id="slidebar-der" class="span4">
+      <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('slidebar_derecha') ) : ?>
+    <?php endif; ?>
+  </div>
+</div>
+</div>      
 <?php get_footer(); ?>  
