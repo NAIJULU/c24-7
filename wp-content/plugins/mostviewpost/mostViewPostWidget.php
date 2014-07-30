f1eac3a8c854538dd8ad6f7bff0d231bc4ec4da3
pablopalillo <pablo.576@hotmail.com>
2014-07-30 11:16:18 -0500

    se corrgio el espacio que unica las fechas en los widgets de clima , mas vistos, mas votados y mas comentados

--- wp-content/plugins/mostviewpost/mostViewPostWidget.php
+++ wp-content/plugins/mostviewpost/mostViewPostWidget.php
@@ -79,7 +79,7 @@ while ($the_query->have_posts() ) : $the_query->the_post();
 	      							'<div class="contenido">'.
 		      						'<span class="categorias">'.strtolower($categoria).'</span>'.
 		      						'<h1>'.the_title('','',false).'</h1>'.
-		      						'<time datetime="'.get_the_time('Y-m-j').'" pubdate>'.get_the_time('j').'de '.get_the_time('F').' del '.get_the_time('Y').'</time>'.
+		      						'<time datetime="'.get_the_time('Y-m-j').'" pubdate>'.get_the_time('j').' de '.get_the_time('F').' del '.get_the_time('Y').'</time>'.
 	      							'</div></a></article>';
      }
         endif;
