7799e4c0ee3a1d81e96ee1de0532ce99f48fafc2
pablopalillo <pablo.576@hotmail.com>
2014-10-24 18:02:42 -0500

    avance formulario widget clima

--- wp-content/themes/clima/widget.php
+++ wp-content/themes/clima/widget.php
@@ -7,6 +7,45 @@ get_header();
 
 <div id="content" class="clearfix row-fluid">
   <div id="widget" class="span12 clearfix" role="main">
+
+    <div class="span6">
+        <p> incluye en tu web, el widget de clima 24/7 </p>
+        <form class="form-horizontal">
+            <div class="control-group">
+                <label class="control-label" for="inputOpciones">Pronostico: </label>
+                <div class="controls">
+                     <label class="checkbox">
+                        Lluvias  <input type="checkbox" name="opciones" value="1">
+                    </label>
+                    <label class="checkbox">
+                        Temperatura <input type="checkbox" name="opciones" value="2">
+                    </label>
+                </div>
+            </div>
+
+            <div class="control-group">
+                <label class="control-label" for="inputOpciones">Tema: </label>
+                <div class="controls">
+                    <select name="tema">
+                        <option value="1">Claro (defecto)</option>
+                        <option value="2">Oscuro</option>
+                        <option value="3">Crema</option>
+                        <option value="4">Frutas tropiclaes</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="control-group">
+                <label class="control-label" for="inputOpciones">Tamaño máximo (px): </label>
+                <div class="controls">
+                  <input type="text" name="max" placeholder="Maximo 350px">
+                </div>
+            </div>
+
+
+        </form>
+    </div>
+
     <div class="span6">
     	<article class="p-lluvia">
     		<header>
