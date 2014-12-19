e1c3172e1a0a41cc818ad95934e828d7d11b78ba
pablopalillo <pablo.576@hotmail.com>
2014-12-19 18:28:18 -0500

    administrador visor widget compeltado

--- wp-content/plugins/widgetview/widgetview.php
+++ wp-content/plugins/widgetview/widgetview.php
@@ -18,7 +18,6 @@ if (!defined('WP_PLUGIN_DIR'))
 
 
 add_action( 'admin_menu', 'admin_widget' );
-//add_action( 'widgets_init', 'creaWidgetView' );
 
 function admin_widget()
 {
@@ -29,14 +28,51 @@ function admin_widget()
 function adminView()
 {	
 
+    global $wpdb;
 
+    $total =    null;
+    $max_pag =  null;
+    $limit =    null;
 
-    global $wpdb;  
+    $pag = isset($_GET['pagina']) ? $_GET['pagina'] : 1;
 
-    $query    = "SELECT cliente,correo,web,fecha FROM c247_widget;";
+    $query        = "SELECT count(*) AS total, TRUNCATE( (count(*) / 30), 0 ) AS pag from c247_widget ;";
+    $info_query   = $wpdb->get_results($query);
+
+    foreach ($info_query as $key => $value) 
+    {
+      $total    = $value->total;
+      $max_pag  = $value->pag;
+    }
+
+    if( $total <= 30 )
+    {
+      $limit = '';
+    }
+    else
+    {
+
+        if( $pag == 1 )
+        {
+          $limit = 'LIMIT 30';
+        }
+        else
+        {
+          if( $pag > 1)
+          {
+            $incr  =  ($pag-1) * 30; 
+            $limit = "LIMIT 30 , $incr "; 
+          }
+
+        }
+
+    }
+
+    $query    = "SELECT cliente,correo,web,fecha FROM c247_widget ORDER BY fecha DESC $limit;";
     $results  = $wpdb->get_results($query);
     $ciudad   = array();
     $columns  = "";
+
     foreach ($results as $key => $value) 
     {
       $columns .= "<tr>";
@@ -46,22 +82,41 @@ function adminView()
       $columns .= "<td> $value->fecha </td>";
       $columns .= "</tr>";
     }
-
-    
 ?>
-  
+  <link href="<?php echo WP_PLUGIN_URL.'/widgetview/widgetadmin.css' ?>" rel="stylesheet">
   <div class="widget-admin">
-    <table>
-    <tr>
+    <table class="tabla-widget">
+    <tr class="enc">
       <th>Cliente</th>
-      <th>Correo</th>
-      <th>Web</th>
+      <th class="medium">Correo</th>
+      <th class="medium">Web</th>
       <th>fecha</th>
     </tr>
 
     <?php echo $columns ?>
       
     </table>
+
+
+
+    <div class="paginador">
+      <div class="info">
+        <span>Total </span> <span> <?php echo $total ?></span>
+      </div>
+        <div class ="area-izquierda">
+          <?php if( $pag > 1 ): ?>
+            <?php $incr = $pag - 1 ; ?>
+            <a href="<?php echo 'http://localhost/c24-7/wp-admin/options-general.php?page=adminView&pagina='.$incr ?>" class="ant"> Anterior  </a> 
+          <?php endif; ?>
+        </div>
+        <div class="area-derecha">
+          <?php if( ( $pag * 30) < $total ): ?>
+          <?php $incr = $pag +1 ; ?>
+
+            <a href="<?php echo 'http://localhost/c24-7/wp-admin/options-general.php?page=adminView&pagina='.$incr ?>" class="sig"> Siguiente  </a> 
+          <?php endif; ?>
+        </div>
+    </div>
   </div>
 
 <?php
