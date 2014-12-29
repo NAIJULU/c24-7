c1555e8c9e5021023844addf24ef69d41e721db1
Emerson Gutierrez <emergugue@gmail.com>
2014-12-29 11:53:11 -0500

    Se agregó el gitignore en en el equipo de Emerson

--- wp-content/themes/clima/embed.php
+++ wp-content/themes/clima/embed.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset='utf-8'><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'>
 		<script  type='text/javascript' src='http://localhost/c24-7/wp-content/themes/clima/library/js/libs/jquery-1.7.1.min.js'></script>
-
+		<link rel="stylesheet" href="styles-widget.css">
 		<script type="text/javascript">
 			$( document ).ready(function() {
 
